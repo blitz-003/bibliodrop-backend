1753d3dc5fa6d84612348ce12e67550f36c914d8
blitz-003 <elmanpathan@gmail.com>
2026-06-25 23:06:11 +0600

    added some cookie debugging

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -2,8 +2,11 @@ const { getSession } = require("../services/auth.service");
 
 async function requireAuth(req, res, next) {
   try {
+    console.log("COOKIE HEADER:", req.headers.cookie);
+
     const session = await getSession(req.headers);
-    console.log(session);
+
+    console.log("SESSION:", session);
 
     if (!session?.user) {
       return res.status(401).json({
